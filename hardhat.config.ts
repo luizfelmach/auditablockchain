5ab9e0ba87ba9f5ef437a0da41ef6dc7d1117c22
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2024-10-15 09:16:04 -0300

    :sparkles: add tasks to store and recover hashes

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,5 @@
 import { HardhatUserConfig, task } from "hardhat/config";
+import { Auditability } from "./typechain-types/Auditability";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
@@ -9,7 +10,8 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
-    console.log(account.address);
+    const balance = await hre.ethers.provider.getBalance(account.address);
+    console.log(account.address, hre.ethers.formatEther(balance), "ETH");
   }
 });
 
@@ -18,6 +20,26 @@ task("balance", "Prints an account's balance")
   .setAction(async (taskArgs, hre) => {
     const balance = await hre.ethers.provider.getBalance(taskArgs.account);
     console.log(hre.ethers.formatEther(balance), "ETH");
+  });
+
+task("store", "Store hash on node")
+  .addPositionalParam("hash", "Hash to store")
+  .addParam("smart", "The smart contract's address")
+  .setAction(async (taskArgs, hre) => {
+    const { hash, smart } = taskArgs;
+    const Auditability = await hre.ethers.getContractFactory("Auditability");
+    const auditability = (await Auditability.attach(smart)) as Auditability;
+    await auditability.storeHash(hash);
+  });
+
+task("recover", "Recover hash on node")
+  .addPositionalParam("hash", "Hash to store")
+  .addParam("smart", "The smart contract's address")
+  .setAction(async (taskArgs, hre) => {
+    const { hash, smart } = taskArgs;
+    const Auditability = await hre.ethers.getContractFactory("Auditability");
+    const auditability = (await Auditability.attach(smart)) as Auditability;
+    console.log(await auditability.recoverHash(hash));
   });
 
 export default config;
