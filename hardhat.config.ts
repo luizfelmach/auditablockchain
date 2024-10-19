e8af605a83ebd640dd8012530a0daea8579ca246
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2024-10-19 17:58:52 -0300

    :truck: move path root of contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,9 @@
-import { HardhatUserConfig, task } from "hardhat/config";
-import { Auditability } from "./typechain-types/Auditability";
 import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig, task } from "hardhat/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
+  paths: { root: "./contracts" },
 };
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -20,34 +20,6 @@ task("balance", "Prints an account's balance")
   .setAction(async (taskArgs, hre) => {
     const balance = await hre.ethers.provider.getBalance(taskArgs.account);
     console.log(hre.ethers.formatEther(balance), "ETH");
-  });
-
-task("store", "Store Merkle Root on blockchain")
-  .addPositionalParam("index", "Index to map")
-  .addPositionalParam("root", "Merkle Root to store")
-  .addParam("smart", "The smart contract's address")
-  .setAction(async (taskArgs, hre) => {
-    const { index, root, smart } = taskArgs;
-    const Auditability = await hre.ethers.getContractFactory("Auditability");
-    const auditability = Auditability.attach(smart) as Auditability;
-    await auditability
-      .store(index, root)
-      .catch((err) => console.log(err.message));
-  });
-
-task("proof", "Proof off-chain Merkle Root")
-  .addPositionalParam("index", "Index to map")
-  .addPositionalParam("root", "Merkle Root to proof")
-  .addParam("smart", "The smart contract's address")
-  .setAction(async (taskArgs, hre) => {
-    const { index, root, smart } = taskArgs;
-    const Auditability = await hre.ethers.getContractFactory("Auditability");
-    const auditability = Auditability.attach(smart) as Auditability;
-
-    await auditability
-      .proof(index, root)
-      .then(console.log)
-      .catch((err) => console.log(err.message));
   });
 
 export default config;
