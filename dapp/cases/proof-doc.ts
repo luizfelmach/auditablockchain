b4463a941407e91e6a83d0abac4593a4f560c866
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2025-01-20 10:34:50 -0300

    :ambulance: remove merkle tree hasher

--- dapp/cases/proof-doc.ts
+++ dapp/cases/proof-doc.ts
@@ -1,12 +1,11 @@
-import { SimpleMerkleTree } from "@openzeppelin/merkle-tree";
 import { hasher } from "../helpers/hasher";
 import { ElasticService } from "../services/elastic";
 import { contract } from "../providers/rpc";
 
 export async function proofDoc(index: string) {
   const docs = await ElasticService.getDocs(index);
-  const hashes = docs.map((d) => hasher(d));
-  const { root } = SimpleMerkleTree.of(hashes);
-  const result = await contract.proof(index, root);
+  const raw = docs.map((item) => JSON.stringify(item)).join("");
+  const hash = hasher(raw);
+  const result = await contract.proof(index, hash);
   console.log(result);
 }
--- dapp/cases/sync-index.ts
+++ dapp/cases/sync-index.ts
@@ -1,4 +1,3 @@
-import { SimpleMerkleTree } from "@openzeppelin/merkle-tree";
 import { ElasticService } from "../services/elastic";
 import { hasher } from "../helpers/hasher";
 import { IndexNotFoundError } from "../helpers/error";
@@ -10,8 +9,8 @@ export async function syncIndex(index: string) {
     throw new IndexNotFoundError();
   }
   const docs = await ElasticService.getDocs(index); // Will the docs always return in the same order ???
-  const hashes = docs.map((d) => hasher(d));
-  const { root } = SimpleMerkleTree.of(hashes);
-  await contract.store(index, root);
-  console.log(`Merkle Root for '${index}': ${root}`);
+  const raw = docs.map((item) => JSON.stringify(item)).join("");
+  const hash = hasher(raw);
+  await contract.store(index, hash);
+  console.log(`Fingerprint for '${index}': ${hash}`);
 }
